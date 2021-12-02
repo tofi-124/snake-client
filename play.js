96e08412f0ea6a46c0ef008ee7cd62e0a2924308
Tofik Mohammed <tofikahm@yahoo.com>
2021-12-02 04:23:33 +0000

    added keys

--- play.js
+++ play.js
@@ -1,5 +1,22 @@
 const net = require("net");
 const client = require("./client");
 
-console.log("Connecting ...");
-client.clientConnect()
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", (key) => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  });
+
+  return stdin;
+};
+
+setupInput();
+
+// console.log("Connecting ...");
+// client.clientConnect()
